237e90015c9ef3b4b38654cad002a24375bfab20
StevenLiCh <u20304666@utp.edu.pe>
2026-06-19 07:52:22 -0500

    comentar token

--- Endpoints/get_recetas_paciente.py
+++ Endpoints/get_recetas_paciente.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 from database import SessionLocal, Base
 
 # ── NUEVO: importar la dependencia que valida el JWT ──────────────────────────
-from auth.jwt_config import get_doctor_actual
+#from auth.jwt_config import get_doctor_actual
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
